317453cc31e545e45f458d8ff78696b548c1397d
Gustavo Carvalho <gustavo.hcarvalho@outlook.com>
2022-08-25 18:11:24 -0300

    End of section two: - We make a controller feature that can handle and render it on the browser! - Add tests for that controller.

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -6,10 +6,24 @@ module R00lz
   class Error < StandardError; end
 
   class App
-    def call(env) # Like proc#call
-      [200,
-      { 'Content-Type' => 'text/html' },
-      ["Hello from R00lz!"]]
+    def call(env) # Replace prev
+      klass, action = controller_and_action(env)
+      text = klass.new(env).send(action)
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+
+    def controller_and_action(env)
+      _, controller, action, after = env["PATH_INFO"].split('/')
+      controller = controller.capitalize + "Controller"
+      [Object.const_get(controller), action]
+    end
+  end
+
+  class Controller
+    attr_reader :env
+    
+    def initialize(env)
+      @env = env
     end
   end
 end
--- test/test_r00lz.rb
+++ test/test_r00lz.rb
@@ -2,15 +2,17 @@
 
 require "test_helper"
 
+class TedController < R00lz::Controller
+  def think; "Whoah, man..."; end
+end
+
 class TestR00lz < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::R00lz::VERSION
   end
 
-  def test_app_returns_success
-    env = { "PATH_INFO" => "/",
-            "QUERY_STRING" => "" }
-    assert_equal 200,
-    ::R00lz::App.new.call(env)[0]
+  def test_new_controller_action
+    env = { "PATH_INFO" => "/ted/think", "QUERY_STRING" => "" }
+    assert_equal 200, ::R00lz::App.new.call(env)[0]
   end
 end
